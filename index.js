cc90ff9942f598524468e6c6ea17856e624046d3
qihong <qihong@kine.io>
2022-08-11 09:50:47 +0800

    aaa

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 const fetch = require("node-fetch")
  
 // fetch('https://api.binance.com/api/v3/ticker/price', {
-fetch('https://api.binance.com/api/v3/ticker/price', {
+// fetch('https://api.binance.com/api/v3/ticker/price', {
 
-    // https://api.juejin.cn/tag_api/v1/query_category_briefs
-    method: 'GET'
-})
-.then(async res => {
-    let data = await res.json();
+//     // https://api.juejin.cn/tag_api/v1/query_category_briefs
+//     method: 'GET'
+// })
+// .then(async res => {
+//     let data = await res.json();
    
-    console.log(data,'<---data');
-})
-.catch(err => {
-    console.log('Error: ', err.message);
-});
+//     console.log(data,'<---data');
+
+
+// })
+// .catch(err => {
+//     console.log('Error: ', err.message);
+// });
+
+const getprice = async  () => {
+    let data = await fetch('https://api.binance.com/api/v3/ticker/price');
+    console.log(data,'<###');
+}
+
+getprice();
