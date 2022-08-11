db831ae03a4ad113553dfeb8137c7cc50af9a4f5
qihong <qihong@kine.io>
2022-08-11 09:44:04 +0800

    github copilot

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const fetch = require("node-fetch")
+ 
+// fetch('https://api.binance.com/api/v3/ticker/price', {
+fetch('https://api.binance.com/api/v3/ticker/price', {
+
+    // https://api.juejin.cn/tag_api/v1/query_category_briefs
+    method: 'GET'
+})
+.then(async res => {
+    let data = await res.json();
+   
+})
+.catch(err => {
+    console.log('Error: ', err.message);
+});
